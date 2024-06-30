f008b8843b148abb2cd41f5e6773fbd448607323
LostoVayne <moises.barrera.dev@gmail.com>
2024-06-29 21:05:31 -0400

    soluciona el error de ssr de cloudinary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
@@ -8,5 +8,13 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  platformProxy: {
+    enabled: true,
+  },
+  vite: {
+    ssr: {
+      external: ["cloudinary"],
+    },
+  },
 });
