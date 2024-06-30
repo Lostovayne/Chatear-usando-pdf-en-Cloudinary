3edd7140f15c55a9395e4cc45f8d2b0983264e63
LostoVayne <moises.barrera.dev@gmail.com>
2024-06-29 20:54:08 -0400

    agrega adapter para cloudflare server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
+  adapter: cloudflare()
 });
